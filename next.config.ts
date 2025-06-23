187de84ec272adf254004e9e7dd879b2c0f461d6
JJJ <43630709+developerJJJ@users.noreply.github.com>
2025-06-23 00:12:47 -0700

    modify nextjs workflow

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  distDir: "dist",
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
 };
 
 export default nextConfig;
